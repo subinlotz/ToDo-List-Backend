c4b884e2c3ce1a2e75ed38922a203807a951ba24
subinlotz <lotzsubin@gmail.com>
2025-02-03 21:13:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGODB_URL,
                  {
                      useNewUrlParser: true,
                      useUnifiedTopology: true,
-                 });
+                 })
 .then(()=>{
     console.log('connected to the database');
 }).catch((err)=>console.log(err)
